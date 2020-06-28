d628f89211a93b0dbd0a982ecc6529d29a4929c7
Rahul Shah <rahuldshah3@gmail.com>
2020-06-28 05:05:18 -0400

    spain no more

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ESPM50AC-Visualization
+# UC Berkeley ESPM50AC Visualization
 Shows Ethnic Distributions along Coastal areas in response to Natural Disasters
 
-In this project, we worked to create a visualization to 
+In this project, we worked to create a visualization that engaged with conepts from this course such as ethics, ethnic distributions, and natural disasters in an easy to view manner.
--- espmv/static/js/index.js
+++ espmv/static/js/index.js
@@ -4,8 +4,8 @@ const View = ol.View;
 const GeoJSON = ol.format.GeoJSON;
 const Circle = ol.geom.Circle;
 const { Tile: TileLayer, Vector: VectorLayer } = ol.layer;
-const { OSM, Vector : VectorSource } = ol.source;
-const { Circle : CircleStyle, Fill, Stroke, Style } = ol.style;
+const { OSM, Vector: VectorSource } = ol.source;
+const { Circle: CircleStyle, Fill, Stroke, Style } = ol.style;
 
 const map = new Map({
   layers: [
@@ -17,44 +17,53 @@ const map = new Map({
   target: "demoMap",
   view: new View({
     center: [0, 0],
-    zoom: 2
-  })
+    zoom: 2,
+  }),
 });
+map
+  .getView()
+  .setCenter(ol.proj.transform([-119.6, 36.6], "EPSG:4326", "EPSG:3857"))
+map
+  .getView()  
+  .setZoom(6);
+
 
 var styleFunction = function (feature) {
-  console.log(feature.getGeometry().getKeys())
+  // console.log(feature.getGeometry().getKeys());
   return new Style({
     stroke: new Stroke({
-      color: 'blue',
+      color: "blue",
       lineDash: [4],
-      width: 3
+      width: 1,
     }),
     fill: new Fill({
-      color: 'rgba(0, 0, 255, 0.1)'
-    })
-  })
+      color: "rgba(0, 0, 255, 0.1)",
+    }),
+  });
   // return styles[feature.getGeometry().getType()];
 };
-
-
 
 axios
   .get("/static/data/counties.json")
   .then((res) => {
-    console.log('county map dl complete ')
+    console.log("county map dl complete ");
     const countiesData = res.data;
     const geoJsonObj = {
-      type: 'FeatureCollection', 
-      features: []
+      type: "FeatureCollection",
+      features: [],
     };
-    geoJsonObj.features = countiesData.map((county) => ({
-      type: "Feature",
-      geometry: {
-        type: "Polygon",
-        coordinates: county.geometry.map(c => c.map(p => parseFloat(p)))
-      },
-    }));
-    console.log(geoJsonObj);
+    geoJsonObj.features = countiesData.map((county) => {
+      const coords = JSON.parse(
+        JSON.stringify(county.geometry.map((c) => c.map((p) => parseFloat(p))))
+      );
+      return {
+        type: "Feature",
+        geometry: {
+          type: "Polygon",
+          coordinates: [coords],
+        },
+      };
+    });
 
     var vectorSource = new VectorSource({
       features: new GeoJSON({
@@ -62,166 +71,226 @@ axios
         dataProjection: "EPSG:4326",
       }).readFeatures(geoJsonObj),
     });
-    console.log(vectorSource.getFeatures());
 
     var vectorLayer = new VectorLayer({
       source: vectorSource,
-      style: styleFunction
+      style: styleFunction,
     });
-    console.log(vectorLayer.getSource().getFeatures())
 
     map.addLayer(vectorLayer);
-
   })
   .catch((e) => console.log(e));
-
 
 var image = new CircleStyle({
   radius: 5,
   fill: null,
-  stroke: new Stroke({ color: 'red', width: 1 })
+  stroke: new Stroke({ color: "red", width: 1 }),
 });
 
 var styles = {
-  'Point': new Style({
-    image: image
+  Point: new Style({
+    image: image,
   }),
-  'LineString': new Style({
+  LineString: new Style({
     stroke: new Stroke({
-      color: 'green',
-      width: 1
-    })
+      color: "green",
+      width: 1,
+    }),
   }),
-  'MultiLineString': new Style({
+  MultiLineString: new Style({
     stroke: new Stroke({
-      color: 'green',
-      width: 1
-    })
+      color: "green",
+      width: 1,
+    }),
   }),
-  'MultiPoint': new Style({
-    image: image
+  MultiPoint: new Style({
+    image: image,
   }),
-  'MultiPolygon': new Style({
+  MultiPolygon: new Style({
     stroke: new Stroke({
-      color: 'yellow',
-      width: 1
+      color: "yellow",
+      width: 1,
     }),
     fill: new Fill({
-      color: 'rgba(255, 255, 0, 0.1)'
-    })
+      color: "rgba(255, 255, 0, 0.1)",
+    }),
   }),
-  'Polygon': new Style({
+  Polygon: new Style({
     stroke: new Stroke({
-      color: 'blue',
+      color: "blue",
       lineDash: [4],
-      width: 3
+      width: 3,
     }),
     fill: new Fill({
-      color: 'rgba(0, 0, 255, 0.1)'
-    })
+      color: "rgba(0, 0, 255, 0.1)",
+    }),
   }),
-  'GeometryCollection': new Style({
+  GeometryCollection: new Style({
     stroke: new Stroke({
-      color: 'magenta',
-      width: 2
+      color: "magenta",
+      width: 2,
     }),
     fill: new Fill({
-      color: 'magenta'
+      color: "magenta",
     }),
     image: new CircleStyle({
       radius: 10,
       fill: null,
       stroke: new Stroke({
-        color: 'magenta'
-      })
-    })
+        color: "magenta",
+      }),
+    }),
   }),
-  'Circle': new Style({
+  Circle: new Style({
     stroke: new Stroke({
-      color: 'red',
-      width: 2
+      color: "red",
+      width: 2,
     }),
     fill: new Fill({
-      color: 'rgba(255,0,0,0.2)'
-    })
-  })
+      color: "rgba(255,0,0,0.2)",
+    }),
+  }),
 };
 
-
 var geojsonObject = {
-  'type': 'FeatureCollection',
-  'crs': {
-    'type': 'name',
-    'properties': {
-      'name': 'EPSG:3857'
-    }
-  },
-  'features': [{
-    'type': 'Feature',
-    'geometry': {
-      'type': 'Point',
-      'coordinates': [0, 0]
-    }
-  }, {
-    'type': 'Feature',
-    'geometry': {
-      'type': 'LineString',
-      'coordinates': [[4e6, -2e6], [8e6, 2e6]],
-      some_val: 1,
-      data: {
-        density: 100
-      }
+  type: "FeatureCollection",
+  crs: {
+    type: "name",
+    properties: {
+      name: "EPSG:3857",
     },
-    
-  }, {
-    'type': 'Feature',
-    'geometry': {
-      'type': 'LineString',
-      'coordinates': [[4e6, 2e6], [8e6, -2e6]]
-    }
-  }, {
-    'type': 'Feature',
-    'geometry': {
-      'type': 'Polygon',
-      'coordinates': [[[-5e6, -1e6], [-4e6, 1e6], [-3e6, -1e6]]]
-    }
-  }, {
-    'type': 'Feature',
-    'geometry': {
-      'type': 'MultiLineString',
-      'coordinates': [
-        [[-1e6, -7.5e5], [-1e6, 7.5e5]],
-        [[1e6, -7.5e5], [1e6, 7.5e5]],
-        [[-7.5e5, -1e6], [7.5e5, -1e6]],
-        [[-7.5e5, 1e6], [7.5e5, 1e6]]
-      ]
-    }
-  }, {
-    'type': 'Feature',
-    'geometry': {
-      'type': 'MultiPolygon',
-      'coordinates': [
-        [[[-5e6, 6e6], [-5e6, 8e6], [-3e6, 8e6], [-3e6, 6e6]]],
-        [[[-2e6, 6e6], [-2e6, 8e6], [0, 8e6], [0, 6e6]]],
-        [[[1e6, 6e6], [1e6, 8e6], [3e6, 8e6], [3e6, 6e6]]]
-      ]
-    }
-  }, {
-    'type': 'Feature',
-    'geometry': {
-      'type': 'GeometryCollection',
-      'geometries': [{
-        'type': 'LineString',
-        'coordinates': [[-5e6, -5e6], [0, -5e6]]
-      }, {
-        'type': 'Point',
-        'coordinates': [4e6, -5e6]
-      }, {
-        'type': 'Polygon',
-        'coordinates': [[[1e6, -6e6], [2e6, -4e6], [3e6, -6e6]]]
-      }]
-    }
-  }]
+  },
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [0, 0],
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "LineString",
+        coordinates: [
+          [4e6, -2e6],
+          [8e6, 2e6],
+        ],
+        some_val: 1,
+        data: {
+          density: 100,
+        },
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "LineString",
+        coordinates: [
+          [4e6, 2e6],
+          [8e6, -2e6],
+        ],
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [-5e6, -1e6],
+            [-4e6, 1e6],
+            [-3e6, -1e6],
+          ],
+        ],
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "MultiLineString",
+        coordinates: [
+          [
+            [-1e6, -7.5e5],
+            [-1e6, 7.5e5],
+          ],
+          [
+            [1e6, -7.5e5],
+            [1e6, 7.5e5],
+          ],
+          [
+            [-7.5e5, -1e6],
+            [7.5e5, -1e6],
+          ],
+          [
+            [-7.5e5, 1e6],
+            [7.5e5, 1e6],
+          ],
+        ],
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "MultiPolygon",
+        coordinates: [
+          [
+            [
+              [-5e6, 6e6],
+              [-5e6, 8e6],
+              [-3e6, 8e6],
+              [-3e6, 6e6],
+            ],
+          ],
+          [
+            [
+              [-2e6, 6e6],
+              [-2e6, 8e6],
+              [0, 8e6],
+              [0, 6e6],
+            ],
+          ],
+          [
+            [
+              [1e6, 6e6],
+              [1e6, 8e6],
+              [3e6, 8e6],
+              [3e6, 6e6],
+            ],
+          ],
+        ],
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "GeometryCollection",
+        geometries: [
+          {
+            type: "LineString",
+            coordinates: [
+              [-5e6, -5e6],
+              [0, -5e6],
+            ],
+          },
+          {
+            type: "Point",
+            coordinates: [4e6, -5e6],
+          },
+          {
+            type: "Polygon",
+            coordinates: [
+              [
+                [1e6, -6e6],
+                [2e6, -4e6],
+                [3e6, -6e6],
+              ],
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
 
 geojsonObject = {
@@ -245,7 +314,6 @@ geojsonObject = {
   ],
 };
 
-
 // var vectorSource = new VectorSource({
 //   features: new GeoJSON({
 //     featureProjection: 'EPSG:3857',
@@ -259,8 +327,6 @@ geojsonObject = {
 // });
 
 // map.addLayer(vectorLayer);
-
-
 
 // new Map({
 //   // layers: [
